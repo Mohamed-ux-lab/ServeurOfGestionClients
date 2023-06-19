7985ca8ba3f17dfdf70af2de2fc3441b709df8b6
Mohamed-ux-lab <koureissym@gmail.com>
2023-06-19 18:05:08 +0000

    First commit

---
+++ src/main/java/com/serveurGestion/com/ServeurGestionOfClientsApplication.java
@@ -0,0 +1,13 @@
+package com.serveurGestion.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionOfClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionOfClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serveurGestion/com/ServeurGestionOfClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serveurGestion.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionOfClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
